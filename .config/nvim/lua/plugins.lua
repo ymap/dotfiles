ea136821674f518ec662335570fe376437274aeb
ymap <ymap.dev@gmail.com>
2025-12-28 13:51:12 +0000

    chore: update deprecated lspconfig usage

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -499,7 +499,7 @@ return {
       local lspconfig = require('lspconfig')
       for server, config in pairs(opts.servers) do
         config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-        lspconfig[server].setup(config)
+        vim.lsp.config(server, config)
       end
 
       vim.filetype.add {
